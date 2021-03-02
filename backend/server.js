3bc99949e6f179f4bd6cce0fa83353669fefc479
Eric Segala Jovelli <ericsegalajovelli@gmail.com>
2021-03-02 16:09:54 -0300

    Create express server
    PORT 8000

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const routes = require('./app/routes/index').routes;
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 8000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+routes(app);
+app.listen(PORT, () => {
+  console.log('NODE server running on '+PORT);
+});

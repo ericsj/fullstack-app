10c0a8dca754ff238ab1a6e6478840f5b9688d0e
Eric Segala Jovelli <ericsegalajovelli@gmail.com>
2021-03-02 13:00:10 -0300

    Import libs to App.js and remove its default return

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import 'fontsource-roboto';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import {FormControl, InputLabel, Select, MenuItem} from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
   );
 }
 

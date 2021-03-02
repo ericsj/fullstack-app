7d875fded920c7970b7458d25cc5516e0328a1d4
Eric Segala Jovelli <ericsegalajovelli@gmail.com>
2021-03-02 15:45:53 -0300

    Create routes script to respond GET requests
    - This scripts fetches data from Chucknorris api and returns it to frontend

---
+++ backend/app/routes/index.js
@@ -0,0 +1,31 @@
+var http = require('request');
+
+exports.routes = function(app) {
+  app.get('/', (request, response) => {
+    response.header({"Access-Control-Allow-Origin": "*"})
+    let query = request.query.query;
+    let category = request.query.category;
+    let url = `https://api.chucknorris.io/jokes/`;
+    if(category != undefined){
+      url += `random?category=${category}`;
+    }
+    else if (query != undefined){
+      url += `search?query=${query}`;
+    }
+    let newArray = []
+    http(url, { json: true }, (err, res, body) => {
+      if (err) { return console.log(err); }
+      let result = body.result;
+      let value = body.value;
+      if (result){
+        Object.keys(result).map(key=>{
+          newArray.push(result[key].value);
+        })
+        response.json(newArray)
+      }
+      else if (value){
+        response.json([value])
+      }
+    })
+  })
+}

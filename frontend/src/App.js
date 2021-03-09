4dd5b9ee803ef0b42522c53876b0e18c8c88c807
Eric Segala Jovelli <ericsegalajovelli@gmail.com>
2021-03-09 12:29:48 -0300

    Remove unnecessary code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'fontsource-roboto';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './themes/theme';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 import SearchButton from './components/SearchButton';
 import DropdownSearch from './components/DropdownSearch';
-import PaperContainer from './components/PaperContainer';
 import PaperGrid from './components/PaperGrid';
 import ResultCount from './components/ResultCount';
 import useStyles from './useStyles.js'
-import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import consumeApi from './util/consumeApi'
 
 function App() {
-  let startingCategories = ["Any","animal","career","celebrity","dev","explicit","fashion","food","history","money","movie","music","political","religion","science","sport","travel"]
+  let categories = ["Any","animal","career","celebrity","dev","explicit","fashion","food","history","money","movie","music","political","religion","science","sport","travel"]
   const classes = useStyles();
   const [isLoaded, setIsLoaded] = useState(false);
   const [query, setQuery] = useState('');
   const [category, setCategory] = useState('');
-  const [categories, setCategories] = useState(startingCategories);
   const [resultsArray, setResultsArray] = useState([]);
 
   return (
